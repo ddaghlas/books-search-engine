c0e8028957a6dd1d05579477ede0e7348d0f78e4
Diana Daghlas <dianadaghlas@yahoo.com>
2021-09-15 23:49:12 -0400

    Update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/lit-citadel-84254', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ddaghlas:Dlee1807@cluster0.ov25h.mongodb.net/googlebooks?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
