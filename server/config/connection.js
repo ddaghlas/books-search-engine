f423feca254742b5112a2aaf0a60636b417672d3
Diana Daghlas <dianadaghlas@yahoo.com>
2021-09-15 23:55:10 -0400

    Creating Heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ddaghlas:Dlee1807@cluster0.ov25h.mongodb.net/googlebooks?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ddaghlas:<password>@cluster0.ov25h.mongodb.net/googlebooks?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
